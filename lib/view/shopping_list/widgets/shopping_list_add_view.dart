0264f818f451b0adf6eeedcf9bb4444a27059647
BerentM <berentm@protonmail.com>
2023-01-25 22:01:02 +0100

    refactor: shopping_list_add_view widget separation

--- lib/view/shopping_list/widgets/shopping_list_add_view.dart
+++ lib/view/shopping_list/widgets/shopping_list_add_view.dart
@@ -13,53 +13,46 @@ class ShoppingListAddView extends StatelessWidget {
       ),
       body: Column(
         children: [
-          stringInputField(context, AppLocalizations.of(context)!.product),
-          stringInputField(context, AppLocalizations.of(context)!.shop),
+          StringInputField(
+              context: context, label: AppLocalizations.of(context)!.product),
+          StringInputField(
+              context: context, label: AppLocalizations.of(context)!.shop),
           Row(children: [
-            numberInputField(context, AppLocalizations.of(context)!.count),
-            const Spacer(flex: 1),
+            NumberInputField(
+                context: context, label: AppLocalizations.of(context)!.count),
+            const Spacer(flex: 4),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                // TODO: implement
-                onPressed: () => {},
-                child: Text(AppLocalizations.of(context)!.save),
-              ),
+              child: SaveButton(context: context),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.red[600]),
-                  overlayColor: MaterialStateProperty.all(Colors.red[300]),
-                ),
-                onPressed: () => Navigator.pop(context),
-                child: Text(AppLocalizations.of(context)!.cancel),
-              ),
-            )
+              child: CancelButton(context: context),
+            ),
+            const Spacer(flex: 1),
           ])
         ],
       ),
     );
   }
+}
 
-  Padding stringInputField(BuildContext context, String label) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextFormField(
-        decoration: InputDecoration(
-          border: const UnderlineInputBorder(),
-          labelText: label,
-        ),
-      ),
-    );
-  }
+class NumberInputField extends StatelessWidget {
+  const NumberInputField({
+    Key? key,
+    required this.context,
+    required this.label,
+  }) : super(key: key);
 
-  Padding numberInputField(BuildContext context, String label) {
+  final BuildContext context;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: ConstrainedBox(
-        constraints: BoxConstraints.tight(const Size(200, 100)),
+        constraints: BoxConstraints.tight(const Size(100, 100)),
         child: TextFormField(
           maxLength: 5,
           keyboardType: TextInputType.number,
@@ -70,6 +63,69 @@ class ShoppingListAddView extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class StringInputField extends StatelessWidget {
+  const StringInputField({
+    Key? key,
+    required this.context,
+    required this.label,
+  }) : super(key: key);
+
+  final BuildContext context;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+          border: const UnderlineInputBorder(),
+          labelText: label,
+        ),
+      ),
+    );
+  }
+}
+
+class CancelButton extends StatelessWidget {
+  const CancelButton({
+    Key? key,
+    required this.context,
+  }) : super(key: key);
+
+  final BuildContext context;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(Colors.red[600]),
+        overlayColor: MaterialStateProperty.all(Colors.red[300]),
+      ),
+      onPressed: () => Navigator.pop(context),
+      child: Text(AppLocalizations.of(context)!.cancel),
+    );
+  }
+}
+
+class SaveButton extends StatelessWidget {
+  const SaveButton({
+    Key? key,
+    required this.context,
+  }) : super(key: key);
+
+  final BuildContext context;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      // TODO: implement
+      onPressed: () => {},
+      child: Text(AppLocalizations.of(context)!.save),
     );
   }
 }
