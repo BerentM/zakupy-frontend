718c664b41bc2726a67218bb3110161f3b48f6e0
BerentM <berentm@protonmail.com>
2023-03-11 13:07:27 +0100

    refactor: remove adding products via shopping list

--- lib/view/shopping_list/shopping_list.dart
+++ lib/view/shopping_list/shopping_list.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:zakupy_frontend/constants/strings.dart';
 import 'package:zakupy_frontend/view/shopping_list/widgets/shopping_list_view.dart';
 
 import 'cubit/shopping_list_cubit.dart';
@@ -14,42 +13,40 @@ class ShoppingList extends StatelessWidget {
     return BlocProvider(
       create: (context) => ShoppingListCubit(),
       child: Scaffold(
-      appBar: AppBar(
-        title: Text(AppLocalizations.of(context)!.shopping_list),
-        actions: [
-          BlocBuilder<ShoppingListCubit, ShoppingListState>(
-            builder: (context, state) {
-              return IconButton(
-                onPressed: () {
-                  if (state is ShoppingListLoaded) {
-                    context.read<ShoppingListCubit>().fillUp(state.currentData);
-                  }
-                },
-                icon: const Icon(Icons.save),
+        appBar: AppBar(
+          title: Text(AppLocalizations.of(context)!.shopping_list),
+          actions: [
+            BlocBuilder<ShoppingListCubit, ShoppingListState>(
+              builder: (context, state) {
+                return IconButton(
+                  onPressed: () {
+                    if (state is ShoppingListLoaded) {
+                      context
+                          .read<ShoppingListCubit>()
+                          .fillUp(state.currentData);
+                    }
+                  },
+                  icon: const Icon(Icons.save),
+                );
+              },
+            ),
+          ],
+        ),
+        body: BlocBuilder<ShoppingListCubit, ShoppingListState>(
+          builder: (context, state) {
+            if (state is ShoppingListInitial) {
+              context.read<ShoppingListCubit>().loadData();
+              return const Center(child: CircularProgressIndicator());
+            }
+            if (state is ShoppingListLoaded) {
+              return ShoppingListView(
+                currentData: state.currentData,
               );
-            },
-          ),
-          IconButton(
-            onPressed: () => Navigator.pushNamed(context, ADD_PRODUCT),
-            icon: const Icon(Icons.add),
-          )
-        ],
+            }
+            return const Scaffold();
+          },
+        ),
       ),
-      body: BlocBuilder<ShoppingListCubit, ShoppingListState>(
-        builder: (context, state) {
-          if (state is ShoppingListInitial) {
-            context.read<ShoppingListCubit>().loadData();
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (state is ShoppingListLoaded) {
-            return ShoppingListView(
-              currentData: state.currentData,
-            );
-          }
-          return const Scaffold();
-        },
-      ),
-    ),
     );
   }
 }
