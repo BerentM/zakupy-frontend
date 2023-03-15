ad0fdcf5bc1449d07273c6428f15dd55478a8ead
BerentM <berentm@protonmail.com>
2023-03-15 22:11:40 +0100

    feat: shopping list search field

--- lib/view/shopping_list/widgets/shopping_list_view.dart
+++ lib/view/shopping_list/widgets/shopping_list_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:zakupy_frontend/constants/strings.dart';
 import 'package:zakupy_frontend/data/models/product_list.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ShoppingListView extends StatefulWidget {
   final ProductList currentData;
@@ -16,6 +17,29 @@ class ShoppingListView extends StatefulWidget {
 class _ShoppingListViewState extends State<ShoppingListView> {
   late List<ProductListElement> shoppingList = widget.currentData.productList;
   int lastPos = 1;
+
+  void _runFilter(String searchedString) {
+    List<ProductListElement> output;
+    if (searchedString.isEmpty) {
+      output = widget.currentData.productList;
+    } else {
+      var productOutput = widget.currentData.productList
+          .where((value) => value.product!
+              .toLowerCase()
+              .contains(searchedString.toLowerCase()))
+          .toList();
+      var sourceOutput = widget.currentData.productList
+          .where((value) => value.source!
+              .toLowerCase()
+              .contains(searchedString.toLowerCase()))
+          .toList();
+      output = [...productOutput, ...sourceOutput];
+    }
+    setState(() {
+      shoppingList = output;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     shoppingList.sort(
@@ -24,35 +48,52 @@ class _ShoppingListViewState extends State<ShoppingListView> {
     shoppingList.sort(
       (a, b) => a.position.compareTo(b.position),
     );
-    return ListView.builder(
-      itemCount: widget.currentData.count,
-      itemBuilder: (context, index) {
-        return ListTile(
-          selected: shoppingList[index].selected,
-          onTap: () => shoppingList[index].selected
-              ? setState(() {
-                  shoppingList[index].position = 0;
-                  shoppingList[index].selected = false;
-                })
-              : setState(() {
-                  lastPos += 1;
-                  shoppingList[index].position = lastPos;
-                  shoppingList[index].selected = true;
-                }),
-          onLongPress: () => Navigator.pushNamed(context, EDIT_SHOPPING,
-              arguments: shoppingList[index]),
-          leading: SizedBox(
-            height: double.infinity, // center icon
-            child: Icon(
-              Icons.shopping_cart,
-              color: shoppingList[index].selected ? Colors.green : Colors.grey,
-            ),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            onChanged: (value) => _runFilter(value),
+            decoration: InputDecoration(
+                labelText: AppLocalizations.of(context)!.search,
+                suffixIcon: const Icon(Icons.search)),
           ),
-          title: Text(shoppingList[index].product!),
-          trailing: Text(shoppingList[index].missingAmount.toString()),
-          subtitle: Text(shoppingList[index].source!),
-        );
-      },
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: shoppingList.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                selected: shoppingList[index].selected,
+                onTap: () => shoppingList[index].selected
+                    ? setState(() {
+                        shoppingList[index].position = 0;
+                        shoppingList[index].selected = false;
+                      })
+                    : setState(() {
+                        lastPos += 1;
+                        shoppingList[index].position = lastPos;
+                        shoppingList[index].selected = true;
+                      }),
+                onLongPress: () => Navigator.pushNamed(context, EDIT_SHOPPING,
+                    arguments: shoppingList[index]),
+                leading: SizedBox(
+                  height: double.infinity, // center icon
+                  child: Icon(
+                    Icons.shopping_cart,
+                    color: shoppingList[index].selected
+                        ? Colors.green
+                        : Colors.grey,
+                  ),
+                ),
+                title: Text(shoppingList[index].product!),
+                trailing: Text(shoppingList[index].missingAmount.toString()),
+                subtitle: Text(shoppingList[index].source!),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
